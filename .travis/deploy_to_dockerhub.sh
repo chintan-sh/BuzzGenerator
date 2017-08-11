3871dd937e35d029d6716393c487aba5612c08a7
Chintan <chintan.shah104@gmail.com>
2017-08-11 16:53:34 -0400

    Bug fix for docker path

--- .travis/deploy_to_dockerhub.sh
+++ .travis/deploy_to_dockerhub.sh
@@ -5,5 +5,5 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t c0defella/buzzgenerator:$TAG .   #$TRAVIS_REPO_SLUG
-docker push c0defella/buzzgenerator:$TAG #$TRAVIS_REPO_SLUG
+docker build -f Dockerfile -t $DOCKER_USER/buzzgenerator:$TAG .   #$TRAVIS_REPO_SLUG
+docker push $DOCKER_USER/buzzgenerator:$TAG #$TRAVIS_REPO_SLUG
